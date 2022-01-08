8f4603aab8f00713a155e2a751753a0a64982f68
Lakhanlal Gupta <lakhanlal211299@gmail.com>
2022-01-08 10:59:01 +0530

    'check'

--- Strel/views.py
+++ Strel/views.py
@@ -4,4 +4,3 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.contrib import messages
